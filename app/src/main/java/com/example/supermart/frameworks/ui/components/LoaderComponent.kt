5b88e31d305bdb8d668dc87884a4025df9fd9807
Marco Moreno <marko.moreno12@gmail.com>
2023-09-01 15:53:09 -0600

    UI update: Loader Animation

---
+++ app/src/main/java/com/example/supermart/frameworks/ui/components/LoaderComponent.kt
@@ -0,0 +1,37 @@
+package com.example.supermart.frameworks.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+@Composable
+fun LoaderComponent() {
+    val isLoading = remember { mutableStateOf(true) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (isLoading.value) {
+            CircularProgressIndicator()
+        }
+
+        LaunchedEffect(isLoading) {
+            delay(2000)
+            isLoading.value = false
+        }
+    }
+}
